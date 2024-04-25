dac63183c3b8c68abb5e821e1a9064f3d9470dd0
Jaap-Henk Hoepman <jhh@cs.ru.nl>
2024-04-25 20:48:24 +0200

    Act as filter when length=0; change state with first note on message if sync=in

--- README.md
+++ README.md
@@ -18,16 +18,14 @@ It can be used to shift notes, adjust note velocities, delay notes, create rhyth
 XOT-MidiFilter is triggered to produce output with every clock tick. This tick is either derived from the running transport (when 'Sync' equals 1/4, 1/8 or 1/16) or triggered with each incoming note on (when 'Sync' equals 'in'). When 'Triplets' is on, the clock is synchronised on triples of the selected rate. 
 
 
-When setting 'Sync' to 'filter' or 'in', an incoming note-ons (i.e. velocity > 0) is immediately processed. In the expressions generating the output (see below), `N[0]` equals the note, `V[0]` its velocity, and `L=0`. 
-
-When 'Sync' is 'in', the length of the note is determined by the length expression. This allows the generation of rhythmic patterns with a grove driven by notes on a MIDI track. When 'Sync' is 'filter', the length of the note is determined by the corresponding note-off message. This turns XOT-MidiFilter to a proper MIDI filter.
+When setting 'Sync' to 'in', an incoming note-ons (i.e. velocity > 0) is immediately processed. In the expressions generating the output (see below), `N[0]` equals the note, `V[0]` its velocity, and `L=1`. This setting allows the generation of rhythmic patterns with a grove driven by notes on a MIDI track. 
 
 When 'Sync' is equal to 1/4, 1/8 or 1/16 (i.e. when XOT-MidiFilter is synced to the transport), incoming notes are stored in a chord. A node is added with a note on message, and removed with a note off message. When a node is added, its velocity is also recorded. XOT-MidiFilter can use the current chord in the expressions that generate its output by referring to `N[i]` for the i-th note, `V[i]` for the velocity of the i-th note, for i between 0 and `L`-1 (i.e. `L` denotes the number of notes in the current chord). Notes in a chord are always stored in increasing note pitch, i.e. `N[i]` is guaranteed to be smaller than `N[j]` when `i` < `j`.
 
 With every tick the following happens:
 
 1. All expressions are read from the input.
-2. The next state `NS` is computed using the state expression.
+2. The next state `NS` is computed using the state expression. (If 'Sync' is 'in' this only happens the first after a note off message was received; this allows chords to share a single state.)
 3. A loop over all notes in the current chord is started, setting an index
    `i` through values `0` to `L`-1 one by one, and performing the following steps. If the current chord is empty (or when 'Sync' equals 'in'), the steps below are executed once, for `i` equals 0.
    
@@ -35,10 +33,10 @@ With every tick the following happens:
    
    - Otherwise, a note is output with pitch defined by the note expression, velocity defined by the velocity expression, length defined by the length expression and delayed as defined by the delay expression.
    
+   - If the length expression returns 0, then the corresponding note off message is used to turn the note off; this only works if 'Sync' equals 'in'. With this setting XOT-MidiFilter acts as a true note filter.
+   
 4. The current tick count is incremented by 1.
 5. The current state `S` is set to the next state `NS` computed earlier.
-
-
 
 Users can define the following expressions.
 
@@ -105,10 +103,32 @@ State
 
 Sync could be set to 1/8.
 
+### Randomly shift an incoming chord
+
+Gate
+: `true`; always produce output
+
+Note
+: `N[i]+NS`; output each note in the chord applying the current shift.
+
+Velocity
+: `V[i]`; keep velocity
+
+Length
+: `0`; notes stay on as long as the key is pressed (i.e. wait for the note off).
+
+Delay
+: `0`; no delay.
+
+State
+: `5*(r()%3)`; assign random 5-semitone shift to state (which stays constant for all notes in current chord)
+
+Sync should be set to 'in'.
+
 ### Arpeggiator (Up)
 
 Gate
-: `i==0`; produce output on every tick, *once* for the chord.
+: `(L > 0) and (i==0)`; produce output on every tick, *once* for the chord (if there is a chord at all). Note that the condition L>0 is required, because in synced mode the gate expression is evaluated at least once, even if no chord is present, and if true, the note expression is evaluated, which would fail if there is no chord present. 
 
 Note
 : `N[t % L]`; output each note in the chord one by one each tick
--- XOT-MidiFilter.lua
+++ XOT-MidiFilter.lua
@@ -1,37 +1,27 @@
 this.inlets = 1
 this.outlets = 4 -- note on, delay on, note off, delay off
 
--- scripts
+-- state
 
-gatescript = 'false'
-notescript = '60'
-velocityscript = '60'
-lengthscript = '40'
-delayscript = '0'
-statescript = '0'
+S = 0 -- current state
+NS = 0 -- next state
+t = 0 -- tick count
+i = 0 -- note index
+L = 0 -- no. of note in current chord
+N = {} -- current chord notes (0..l-1)
+N[0] = 0
+V = {} -- current chord velocities (0..l-1)
 
-function setgatescript(s)
-    gatescript = s
-end
-
-function setnotescript(s)
-    notescript = s
-end
-
-function setvelocityscript(s)
-    velocityscript = s
-end
-
-function setlengthscript(s)
-    lengthscript = s
-end
-
-function setdelayscript(s)
-    delayscript = s
-end
-
-function setstatescript(s)
-    statescript = s
+function reset(v)
+    L = 0
+    N = {}
+    N[0] = 0
+    V = {}
+    t = 0
+    S = 0
+    NS = 0
+    math.randomseed(os.time())
+    tick(0)
 end
 
 -- parameters
@@ -52,25 +42,37 @@ function setp3(v)
     p3 = v
 end 
 
--- state
+-- scripts
 
-S = 0 -- current state
-NS = 0 -- next state
-t = 0 -- tick count
-i = 0 -- note index
-L = 0 -- no. of note in current chord
-N = {} -- current chord notes (0..l-1)
-V = {} -- current chord velocities (0..l-1)
+gf = loadstring("return false")
+nf = loadstring("return 0")
+vf = loadstring("return 0")
+lf = loadstring("return 0")
+df = loadstring("return 0")
+sf = loadstring("return 0")
 
-function reset(v)
-    L = 0
-    N = {}
-    V = {}
-    t = 0
-    S = 0
-    NS = 0
-    math.randomseed(os.time())
-    tick(0)
+function setgatescript(s)
+    gf = loadstring("return " .. s)
+end
+
+function setnotescript(s)
+    nf = loadstring("return " .. s)
+end
+
+function setvelocityscript(s)
+    vf = loadstring("return " .. s)
+end
+
+function setlengthscript(s)
+    lf = loadstring("return " .. s)
+end
+
+function setdelayscript(s)
+    df = loadstring("return " .. s)
+end
+
+function setstatescript(s)
+    sf = loadstring("return " .. s)
 end
 
 -- auxiliary
@@ -102,7 +104,6 @@ end
 
 -- MIDI note on/off
 function list(note,velocity)
-    print(note,velocity)
     k = 0
     while (k < L) and (N[k] < note) do
         k=k+1
@@ -110,7 +111,7 @@ function list(note,velocity)
     if (velocity ~= 0) and ((k == L) or (N[k] ~= note)) then -- insert
         for j=L-1,k,-1 do
             N[j+1]=N[j]
-           V[j+1]=V[j]
+            V[j+1]=V[j]
         end
 	L=L+1
 	N[k]=note
@@ -118,76 +119,65 @@ function list(note,velocity)
     elseif (velocity == 0) and (k < L) and (N[k] == note) then -- delete
         for j=k,L-1 do
             N[j]=N[j+1]
-           V[j]=V[j+1]
+            V[j]=V[j+1]
         end
         L=L-1        
-    end
-end
-
-function tick(x)
-    local sf = loadstring("return " .. statescript)
-    local gf = loadstring("return " .. gatescript)
-    local nf = loadstring("return " .. notescript)
-    local vf = loadstring("return " .. velocityscript)
-    local lf = loadstring("return " .. lengthscript)
-    local df = loadstring("return " .. delayscript)
-    NS = sf()
-    if L == 0 then
-       N[0]=60
-       V[0]=64
-    end
-    for j=0,math.max(0,L-1) do
-        i = j
-        if gf() then 
-            delay = df()
-	        note = nf()
-            outlet(0,math.floor(note),math.floor(vf()))
-            outlet(1,math.floor(delay))
-            outlet(2,math.floor(note),0)
-            outlet(3,math.floor(lf()+delay))
-        end
-    end
-    t = t+1
-    S = NS
-end
-
-function notetick(n,v)
-    if v ~= 0 then
-        N[0]=n
-        V[0]=v
-        L=1
-	    tick(0)
     end
 end
 
 offmap = {} -- record which note off to send for a note on
 delaymap = {} -- record which delay to send for a note on
 
-function filternote(n,v)
-    if v ~= 0 then
-        local sf = loadstring("return " .. statescript)
-        local gf = loadstring("return " .. gatescript)
-        local nf = loadstring("return " .. notescript)
-        local vf = loadstring("return " .. velocityscript)
-        local lf = loadstring("return " .. lengthscript)
-        local df = loadstring("return " .. delayscript)
-        N[0]=n
-        V[0]=v
-        L=1
-        NS = sf()
-        i = 0
+function dotick()
+    for j=0, math.max(0,L-1) do -- do it at least once, even if chord is empty
+        i = j
         if gf() then 
+            note = math.floor(nf())
+            velocity = math.floor(vf())
+            length = math.floor(lf())            
             delay = math.floor(df())
-	        out = math.floor(nf())
-	        offmap[n] = out
-	        delaymap[n] = delay
-            outlet(0,out,math.floor(vf()))
+            outlet(0,note,velocity)
             outlet(1,delay)
+            if length > 0 then
+                outlet(2,note,0)
+                outlet(3,length+delay)
+            else
+                offmap[N[j]] = note
+                delaymap[N[j]] = delay
+            end
+        else
+            offmap[N[j]] = nil
         end
-        t = t+1
-        S = NS
+    end
+    t = t+1
+end
+
+function tick()
+    NS = sf()
+    dotick()
+    S = NS
+end
+
+lastvelocitywaszero = true
+
+function notetick(n,v)
+    if v ~= 0 then
+        if lastvelocitywaszero then
+	    NS = sf()
+	end
+        N[0] = n
+        V[0] = v
+        L = 1
+        dotick() -- if length == 0, then offmap[n] will be set
+        if lastvelocitywaszero then
+	    S = NS
+	end
+	lastvelocitywaszero = false
     else
-        outlet(2,offmap[n],0)
-        outlet(3,delaymap[n])
+        lastvelocitywaszero = true
+        if offmap[n] ~= nil then
+             outlet(2,offmap[n],0)
+             outlet(3,delaymap[n])
+	end
     end
 end
